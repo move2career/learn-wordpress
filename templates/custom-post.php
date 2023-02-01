6ebf961c6856c962ef1487a6184159a03b71500a
move2career <move2career@gmail.com>
2023-02-01 20:11:52 +0530

    update custom-post code

--- templates/custom-post.php
+++ templates/custom-post.php
@@ -8,14 +8,50 @@
 
 <section class="default-blogs">
     <div class="container">
-        
+        <div class="row">
+            <div class="col-12">
+                <div class="category-list pb-50 d-flex justify-content-center">                    
+                    <ul class="list-unstyled d-flex d-flex-wrap">
+                        <?php
+                            $categories = get_categories();
+                            foreach($categories as $category){
+                                echo '<li><a href="'. get_category_link($category->term_id) .'">'. $category->name .'</a></li>';
+                            }
+                        ?>                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="category-list pb-50 d-flex justify-content-center">                    
+                    <ul class="list-unstyled d-flex d-flex-wrap">
+                        <?php
+                            $tags = get_tags();
+                            foreach($tags as $tag){
+                                echo '<li><a href="'. get_tag_link($tag->term_id) .'">'. $tag->name .'</a></li>';
+                            }
+                        ?>                        
+                    </ul>
+                </div>
+            </div>
+        </div>
         <div class="row">
             <?php
                 query_posts(array(
                     'post_type' => 'post',
                     'posts_per_page' => -1,
-                    'order' => "Asc"
-                ));
+                    'order' => "Asc",
+                    'tax_query' => array(
+                        array(
+                            'taxonomy' => 'category',
+                            'field' => 'slug',
+                            'terms' => array('WordPress'),
+                            'operator' => 'NOT IN'
+                        )
+                    )
+                ));           
+                
             
                 if( have_posts() ): while( have_posts() ) : the_post();
                 
@@ -29,6 +65,8 @@
                     <h5 class="card-title my-0"><?php the_title(); ?></h5>
                     <div class="post-category"><?php the_category(); ?></div>
                     <div class="post-content"><?php the_excerpt(); ?></div>
+                      <div class="post-author"><?php the_field('author_name'); ?></div>
+                      
                     <div class="mt-3">
                         <a href="<?php the_permalink(); ?>" class="btn btn-info btn-lg">View More</a>
                     </div>
